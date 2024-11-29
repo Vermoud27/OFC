f91c0508abed2fa88c9d6f8163e1242950d15022
Mateo Sa <mateo.sa@etu.univ-lehavre.fr>
2024-11-29 13:08:20 +0100

    .env + langage

---
+++ system/Language/fr/Errors.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// Errors language settings
+
+return [
+    'pageNotFound' => '404 - page non trouvée',
+    'sorryCannotFind' => 'Désolé ! Vous ne parvenez pas à trouver la page que vous recherchez.',
+    'whoops' => 'Oups !',
+    'weHitASnag' => 'Il semble que nous ayons rencontré un problème. Veuillez réessayer plus tard...',
+    ];
