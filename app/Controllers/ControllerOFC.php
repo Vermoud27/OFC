b79bc7b28b5cd187ec6f0bec7ab6694171b7d547
Louis <louis.marouard@hotmail.fr>
2024-12-03 14:52:05 +0100

    header + faq

--- app/Controllers/ControllerOFC.php
+++ app/Controllers/ControllerOFC.php
@@ -2,13 +2,21 @@
 
 namespace App\Controllers;
 
-use Config\Pager;
+use App\Models\FAQModel;
 
 class ControllerOFC extends BaseController
 {
     public function index(): string
     {
-        return view('pageAccueil');
+        //return view('pageAccueil');
+
+        $faqModel = new FaqModel(); // Instanciez le modèle
+
+        // Récupérez les 10 premières questions
+        $data['faqs'] = $faqModel->orderBy('id_faq', 'ASC')->findAll(10);
+
+        // Chargez la vue avec les questions
+        return view('pageAccueil', $data);
     }
 }
 
